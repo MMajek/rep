42f0724e030754b12ab1da29ee766693681e14f6
MMajek <majekmichael3@gmail.com>
2023-03-02 12:52:59 +0000

    Create New GamePage

--- game.js
+++
@@ -1,9 +0,0 @@
-function homeScript() {
-    const homeBtn = document.querySelector('#homeButton');
-    homeBtn.addEventListener('click', () => {
-    // go to homepage;
-})}
-
-function getSelectedColor(color){
-    alert(color);
-}
