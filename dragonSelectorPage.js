f45ff699a40aa433d1a4d937d5378a9bd4ba8ae2
MMajek <majekmichael3@gmail.com>
2023-03-02 12:53:13 +0000

    change file names

---
+++ dragonSelectorPage.js
@@ -0,0 +1,10 @@
+function homeScript() {
+    const homeBtn = document.querySelector('#homeButton');
+    homeBtn.addEventListener('click', () => {
+    // go to homepage;
+})}
+
+function getSelectedColor(color){
+    const dragonColor = color;
+    alert(color);
+}
